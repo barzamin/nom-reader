7460bbe0744f034de5d049bcd03d9b3ab87d4d4e
Erin <sylphofelectricity@gmail.com>
2018-05-17 15:41:04 -0500

    update for Nom 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
 extern crate nom;
 
-use std::io::Read;
+use std::io::{self, Read};
 
 
 pub fn read<P, O, E, R>(parser: P, mut rdr: R) -> Result<O, E>
     where R: Read,
-          E: From<std::io::Error> + From<nom::ErrorKind>,
+          E: From<nom::ErrorKind> + From<io::Error>,
           P: Fn(&[u8]) -> nom::IResult<&[u8], O>,
 {
     let mut input: Vec<u8> = Vec::new();
     loop {
-        match parser(&input) {
-            nom::IResult::Done(_, parsed) => return Ok(parsed),
-            nom::IResult::Error(err) => return Err(E::from(err)),
-            nom::IResult::Incomplete(needed) => {
-                let len = match needed {
-                    nom::Needed::Unknown => input.len() + 1,     // read one byte
+        let to_read = match parser(&input) {
+            Ok((_, parsed)) => return Ok(parsed),
+            Err(nom::Err::Incomplete(needed)) => {
+                match needed {
+                    nom::Needed::Unknown => 1,     // read one byte
                     nom::Needed::Size(len) => len,
-                };
-
-                (&mut rdr).take((len - input.len()) as u64).read_to_end(&mut input)?;
+                }
             },
+            Err(e) => return Err(e.into_error_kind().into()),
         };
+
+        (&mut rdr).take(to_read as u64).read_to_end(&mut input)?;
     }
 }
